958fca53b16a815cfa7bb0ab1fd1a0fdc67981e8
Yash Thakur <107828132+yt5499@users.noreply.github.com>
2023-04-07 12:33:54 +0530

    Update CharVar.java

--- varndatatypes/CharVar.java
+++ varndatatypes/CharVar.java
@@ -6,7 +6,7 @@ public class CharVar {
 	public static void main(String[] args) {
 		
 		char ch1,ch2;
-		ch1=88;
+		ch1=88;		//ASCII value of 'X'
 		ch2='Y';
 		
 		System.out.println("Character at ch1 = "+ch1+" and ch2 = "+ch2);
